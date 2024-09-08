be23f65331c7f45deef4ab358870ed8a03808845
EnkayCodes <olisedemjasmine@gmail.com>
2024-09-08 11:46:38 +0100

    Installation and Set-up  Process for CertiLock

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Certificate Issuing Platform API');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
